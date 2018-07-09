17109c37bfe23a74844b9995402cc7ba68f8b189
andrewch8o <andriy.chumachenko@live.com>
2018-07-09 12:53:08 -0700

    Adding solution prior to review

---
+++ L13-left-join/companies-and-acquisitions-by-state.sql
@@ -0,0 +1,14 @@
+-- https://community.modeanalytics.com/sql/tutorial/sql-left-join/
+-- Count the number of unique companies (don't double-count companies) and unique acquired companies by state. 
+--   Do not include results for which there is no state data, 
+--   and order by the number of acquired companies from highest to lowest.
+
+select companies.state_code as state_code,
+       count(distinct companies.permalink) as companies_count,
+       count(distinct acquisitions.company_permalink) as acquired_count
+    from tutorial.crunchbase_companies companies
+    left join tutorial.crunchbase_acquisitions acquisitions 
+           on companies.permalink = acquisitions.company_permalink
+    where companies.state_code <> ''
+    group by companies.state_code
+    order by acquired_count desc
