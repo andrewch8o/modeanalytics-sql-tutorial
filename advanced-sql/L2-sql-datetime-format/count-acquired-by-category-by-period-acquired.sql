9b007ff8be1e9f85ca269e143e30fc33c7680595
andrewch8o <andriy.chumachenko@live.com>
2018-08-03 16:20:09 -0700

    Adjusted implementation after reviewing / investigating solution

--- advanced-sql/L2-sql-datetime-format/count-acquired-by-category-by-period-acquired.sql
+++ advanced-sql/L2-sql-datetime-format/count-acquired-by-category-by-period-acquired.sql
@@ -5,13 +5,13 @@
      Group by category and limit to only rows with a founding date.*/
 SELECT
   companies.category_code as companies_category_code,
-  COUNT(CASE WHEN (TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') - TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY')) < INTERVAL '3 years' 
+  COUNT(CASE WHEN TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') <= TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY') + INTERVAL '3 years' 
             THEN 1
             ELSE NULL END) AS acquired_within_3_yrs_count,
-  COUNT(CASE WHEN (TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') - TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY')) < INTERVAL '5 years' 
+  COUNT(CASE WHEN TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') <= TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY') + INTERVAL '5 years'
             THEN 1
             ELSE NULL END) AS acquired_within_5_yrs_count,
-  COUNT(CASE WHEN (TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') - TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY')) < INTERVAL '10 years' 
+  COUNT(CASE WHEN TO_TIMESTAMP(acquisitions.acquired_at, 'MM/DD/YY') <= TO_TIMESTAMP(companies.founded_at, 'MM/DD/YY') + INTERVAL '10 years'
             THEN 1
             ELSE NULL END) AS acquired_within_10_yrs_count,
   COUNT(companies.permalink) as acquired_all_time_count
@@ -20,3 +20,4 @@ JOIN tutorial.crunchbase_acquisitions acquisitions
 ON companies.permalink = acquisitions.company_permalink
 WHERE companies.founded_at IS NOT NULL
 GROUP BY 1
+ORDER BY 5 DESC
